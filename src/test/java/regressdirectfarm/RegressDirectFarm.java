778fbae3173fb2fe7be7754b1b35d0dcda8dcbb4
Igor Makeenkov <pro100igo228@gmail.com>
2022-08-05 10:43:42 +0300

    Добавил TestNG

--- src/test/java/regressdirectfarm/RegressDirectFarm.java
+++ src/test/java/regressdirectfarm/RegressDirectFarm.java
@@ -6,26 +6,31 @@ import Case.CommentPostPlant;
 import Case.CreatePostPlant;
 import Case.DeletePostPlant;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Allure;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.description;
 import static io.qameta.allure.Allure.step;
 
-public class regressDirectFarm {
+public class RegressDirectFarm {
 
-    @BeforeEach
+
+    @BeforeSuite(description = "Путь к драйверу", inheritGroups = true)
     public void setup(){
-
+        Selenide.open("https://test.direct.farm");
         description("LoggerAllure включен");
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         SelenideLogger.addListener("allure", new AllureSelenide());
@@ -40,9 +45,9 @@ public class regressDirectFarm {
 
         description("Конфигурация системы");
         Configuration.browserCapabilities = capabilities;
-        Configuration.baseUrl = "https://test.direct.farm";
-        open("/");
         Configuration.browserSize = "1920x1080";
+        /*Configuration.baseUrl = "https://test.direct.farm";*/
+        open("/");
         /*Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";*/
 
         description("Аттачи в Allure");
@@ -56,7 +61,7 @@ public class regressDirectFarm {
 
 
     @DisplayName("Авторизация")
-    @Test
+    @BeforeTest(alwaysRun = true, groups = "Authorization" )
     public void authorization(){
         Authorization signin = new Authorization();
         SelenideLogger.addListener("allure", new AllureSelenide());
@@ -93,7 +98,7 @@ public class regressDirectFarm {
             Attach.browserConsoleLogs();
         });
     }
-    
+
     @DisplayName("Растениеводство удаление")
     @Test
     public void deletePostPlant(){
